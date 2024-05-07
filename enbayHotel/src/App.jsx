1c07628bb0024ecd7a9957956999c74bebcef620
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2024-05-07 14:23:28 +0300

    first commit

---
+++ enbayHotel/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/navbar.jsx'
+import './components/navbar.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="app">
+        <Navbar />
+      </div>
+    </>
+  )
+}
+
+export default App
